6638dd9384836bce2d3adf526008a392fc25b254
deepaktomar2031 <deepaktomar2031@gmail.com>
2024-05-31 23:13:34 +0200

    Code Refactor

--- src/controller/fetchData.controller.js
+++ src/controller/fetchData.controller.js
@@ -14,13 +14,12 @@ const getMonitoringData = async (req, res) => {
     const url = "https://api.spacexdata.com/v5/launches/";
     try {
         spaceData = await fetch(url, requestParams);
+        const spaceDataParsed = await spaceData.json();
+        return res.status(statusCode.successful_request).send({ successful: false, Message: message.Fetched_successfully, spaceDataParsed });
     } catch (error) {
         console.log(LogErrorMessage(error));
         return res.status(statusCode.internal_server_error).send({ successful: false, Message: message.Something_went_wrong });
     }
-
-    const spaceDataParsed = await spaceData.json();
-    return res.status(statusCode.successful_request).send({ successful: false, Message: message.Fetched_successfully, spaceDataParsed });
 };
 
 module.exports = getMonitoringData;
